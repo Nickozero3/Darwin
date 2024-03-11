ef12de48d21f509ed530fd0bda6245ee2ac0cd2b
Nicko <Nickozero3@gmail.com>
2024-03-11 17:37:21 -0300

    railway

--- PHP/conexion.php
+++ PHP/conexion.php
@@ -1,11 +1,19 @@
 <?php
 // Configuración de la conexión a la base de datos
 
+// CLEVER CLOUD
+// $host = "ba1kevs6pkfgcauzroh0-mysql.services.clever-cloud.com";
+// $user = "upqvy2lkrkj8hapn";
+// $password = "r7CLnyBrOJWDHIORrjeF";
+// $dataBaseName = "ba1kevs6pkfgcauzroh0";
 
-$host = "ba1kevs6pkfgcauzroh0-mysql.services.clever-cloud.com";
-$user = "upqvy2lkrkj8hapn";
-$password = "r7CLnyBrOJWDHIORrjeF";
-$dataBaseName = "ba1kevs6pkfgcauzroh0";
+// railway
+$host = "viaduct.proxy.rlwy.net";
+$user = "root";
+$password = "GckmGXKHdfHoYOlqUdPEQWOzrpOIOEjT";
+$dataBaseName = "railway";
+
+
 
 // Crear la conexión
 $conn = new mysqli($host, $user, $password, $dataBaseName);
