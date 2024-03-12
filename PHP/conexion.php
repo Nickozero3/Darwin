896e564812877e13e26c6980af63b4d449b10738
Nicko <Nickozero3@gmail.com>
2024-03-12 00:03:23 -0300

    A

--- PHP/conexion.php
+++ PHP/conexion.php
@@ -8,15 +8,27 @@
 // $dataBaseName = "ba1kevs6pkfgcauzroh0";
 
 // railway
-$host = "viaduct.proxy.rlwy.net";
-$user = "root";
-$password = "udRoenMYkjdCqGTpgefMexuKbstfICrJ";
-$dataBaseName = "railway";
+session_start();
+// $host = "viaduct.proxy.rlwy.net";
+// $user = "root";
+// $password = "udRoenMYkjdCqGTpgefMexuKbstfICrJ";
+// $data_Base_Name = "railway";
+// $DB_Port = "x";
+
+// // Crear la conexión
+// $conn = new mysqli($host, $user, $password, $data_Base_Name, $DB_Port);
+
+
+$DB_HOST = $_ENV['DB_HOST'];
+$DB_USER = $_ENV['DB_USER'];
+$DB_PASSWORD = $_ENV['DB_PASSWORD'];
+$DB_NAME = $_ENV['DB_NAME'];
+$DB_PORT = $_ENV['DB_PORT'];
 
 
 
 // Crear la conexión
-$conn = new mysqli($host, $user, $password, $dataBaseName);
+$conn = new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME, $DB_PORT);
 
 
 // Verificar la conexión
